76500691c8d9400290aa9abac5ded6542438b4bc
Luke Evers <me@lukevers.com>
2013-09-12 16:49:27 -0400

    Added a config file and the main func file.

---
+++ config.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"encoding/json"
+)
+
+type Config struct {
+	
+	// Port is an integer that defines what port to run the
+	// webserver for ocelot on
+	Port int
+	
+}
---
+++ ocelot.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/inhies/go-log"
+	"fmt"
+	"os"
+)
+
+var (
+	l *log.Logger
+	LogLevel = Log.LogLevel(log.INFO)
+	LogFlags = log.Ldate | log.Ltime
+	LogFile  = os.Stdout
+)
+
+func main() {
+	
+	// Load the configuration file
+	config, err := ReadConfig("example.config.json")
+	if err != nil {
+		fmt.Printf("Could not read configuration file: %s", err)
+		os.Exit(1)
+	}
+	
+	// Start the logger
+	l, err = log.NewLevel(LogLevel, true, LogFile, "", LogFlags)
+	if err != nil {
+		fmt.Printf("Could not start logger: %s", err)
+		os.Exit(1)
+	}
+	
+	// Do shit here!
+	
+	
+	
+}
