c35986b8a93b314929a3599c85a6ec0a164b634e
Luke Evers <me@lukevers.com>
2013-09-19 11:45:27 -0400

    Removed cog.js
    Instead of making lightboxes for all of the sidelinks, I thought it'd
    be better to give each its own page.

--- assets/js/cog.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#settings').bind('click', showSettings);
-});
-
-// The function showSettings drops down a lightbox with the settings
-// panel inside so users can update their settings.
-function showSettings() {
-    
-}
